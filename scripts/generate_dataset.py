c8f6c157d916b17adc650d7b39acd61be35e7e76
Emirhan Dogan <emirhan.dogan@gx29.hpc.sci.hpi.de>
2026-05-28 22:57:47 +0200

    generated dataset

--- scripts/generate_dataset.py
+++ scripts/generate_dataset.py
@@ -29,11 +29,16 @@ def prepare_prompt(sample):
     # Hint: the input is the samples "output"
     # Remember the prompt-postfix if you use Qwen!
     # return as "enhancement_prompt"
+    
+    prompt_template = choice(list((pp.DATA_DIR / "prompts").glob("*.txt"))).read_text()
+    prompt = replace_pattern(prompt_template, "input", sample["output"])
+    
+    prompt = replace_pattern(prompt, "tool_start", pp.CONFIG["toolcalling"]["tool_start"])
+    prompt = replace_pattern(prompt, "tool_end", pp.CONFIG["toolcalling"]["tool_end"])
+    prompt = replace_pattern(prompt, "tool_call", pp.CONFIG["toolcalling"]["tool_call"])
 
-    prompt = replace_pattern(prompt, "tool_start", pp.CONFIG["tool_start"])
-    prompt = replace_pattern(prompt, "tool_end", pp.CONFIG["tool_end"])
-    prompt = replace_pattern(prompt, "tool_call", pp.CONFIG["tool_call"])
-    prompt = replace_pattern(prompt, "prompt_postfix", pp.CONFIG["prompt_postfix"])
+    if "qwen" in pp.CONFIG["model"].lower():
+        prompt += pp.CONFIG["dataset"]["qwen"]["prompt_postfix"]
 
     return {"enhancement_prompt": prompt}
 
@@ -46,10 +51,10 @@ def enhance_outputs(dataset, llm, tokenizer):
         # generate a continuation for the prompt using the llm
         # remember to set a reasonable limit for max_new_tokens, and set good parameters for the generation.
         # Hint: use llm.generate
-        generated = llm.generate(prompt, max_new_tokens=100, temperature=0.1, top_k=5)
+        generated = llm.generate(prompt, max_new_tokens=256, temperature=0.1, top_k=5)
         row["enhanced_output"] = generated
         if i%5==4:
-            save_dataset(Dataset.from_list(dataset[:i]))
+            save_dataset(Dataset.from_list(dataset[:i+1]))
     return Dataset.from_list(dataset)
 
 
